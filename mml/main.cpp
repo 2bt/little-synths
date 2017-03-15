7aee3c0ac78201f7d64d765627e820b70827f18a
twobit <s8572327@gmail.com>
2017-03-15 11:54:12 +0100

    stuffy stuff

--- mml/main.cpp
+++ mml/main.cpp
@@ -46,35 +46,38 @@ struct EnvVal {
 const char* tune = R"(
 
 
-#IlQ6L2c<c>d<c>d+<c1c1>d<c>c<c>f<c>d+<c>d<c1c1
-#IpA3>c20r4<A4a+8
-#Ib<<cccccccc
-#
-#IlQ6L2c<c>d<c>d+<c1c1>f<c>d+<c>d<c>c<ca+c1c1
-#IpA3>c20r4<A4a+8
-#Ib<<cccccccc
-#
-#IlQ6L2c<c>d<c>d+<c1c1>d<c>c<c>f<c>d+<c>d<c1c1
-#IpA3>c20r4<A4a+8
-#Ib<<<g+g+g+g+g+g+g+g+
-#
-#IlQ6L2c<c>d<c>d+<c1c1>f<c>d+<c>d<c>c<ca+c1c1
-#IpA3>c20r4<A4a+8
-#Ib<<<g+g+g+g+a+a+a+a+
+IlQ6L2c<c>d<c>d+<c1c1>d<c>c<c>f<c>d+<c>d<c1c1
+IpA3>c20r4<A4a+8
+Q7Ib<<cccccccc
+a+30f2
+
+IlQ6L2c<c>d<c>d+<c1c1>f<c>d+<c>d<c>c<ca+c1c1
+IpA3>c20r4<A4a+8
+Q7Ib<<cccccccc
+g26g2a+2g2
+
+IlQ6L2c<c>d<c>d+<c1c1>d<c>c<c>f<c>d+<c>d<c1c1
+IpA3>c20r4<A4a+8
+Q7Ib<<<g+g+g+g+g+g+g+g+
+>d+3d3<a+2>c18c2<g2f34/r2
+
+IlQ6L2c<c>d<c>d+<c1c1>f<c>d+<c>d<c>c<ca+c1c1
+IpA3>c20r4<A4a+8
+Q7Ib<<<g+g+g+g+a+a+a+a+
 
 # Laxity - Basic
 
-IlL2Q7>e12def4ec4d4 e14deA1f4Aec4d4 d17A2d3A<g>cdA1eAdc<g> e16f8g8
-IbQ4O2L2 cc4cr6c <a+4a+4r>a+4<a+ aa4ar6a g+4g+4r>g+4<g+ gg4gr6g >d4d4r>d4<d cc4cr6c <f4f4r>g4<g
-IpL16Q7<g/>c/e<g+/>c/f<g/>c/e <g+/>c/f< a+/>c/f<a/>c/f<g/>c/eL8<f/a/>c<g/b/>d
-
-IlL2Q7>e12def4ec4d4 e14deA1f4Aec4d4 d17A2d3A<g>cdA1eAdc<g> e16f8g8
-IbQ4O2L2 cc4cr6c <a+4a+4r>a+4<a+ aa4ar6a g+4g+4r>g+4<g+ gg4gr6g >d4d4r>d4<d cc4cr6c <f4f4r>g4<g
-IpL16Q7<g/>c/e<g+/>c/f<g/>c/e <g+/>c/f< a+/>c/f<a/>c/f<g/>c/eL8<f/a/>c<g/b/>d
-
-IlL2Q7>g10>A1dA<b>c6<b3r1b4a3r1A1a3Ag3ad+1d1c6r8Q8d+8Q7e16g+4a4b8b1a1Q8g8Q7arg4a
-IbQ4O2L2 ee4er6e <a4a4r>a4<a >dd4dr6d <a+4a+4r>a+4<a+ aa4ar6a g+4g+4r>g+4<g+ gg4gr6g g4g4r>g4<g
-IpL16Q7<b/>d/g<g/>c/e<a/>c/f<g+/>c/f<g/>c/e<f+/>c/e<f/>c/e<g/b/>d
+#IlL2Q7>e12def4ec4d4 e14deA1f4Aec4d4 d17A2d3A<g>cdA1eAdc<g> e16f8g8
+#IbQ4O2L2 cc4cr6c <a+4a+4r>a+4<a+ aa4ar6a g+4g+4r>g+4<g+ gg4gr6g >d4d4r>d4<d cc4cr6c <f4f4r>g4<g
+#IpL16Q7<g/>c/e<g+/>c/f<g/>c/e <g+/>c/f< a+/>c/f<a/>c/f<g/>c/eL8<f/a/>c<g/b/>d
+#
+#IlL2Q7>e12def4ec4d4 e14deA1f4Aec4d4 d17A2d3A<g>cdA1eAdc<g> e16f8g8
+#IbQ4O2L2 cc4cr6c <a+4a+4r>a+4<a+ aa4ar6a g+4g+4r>g+4<g+ gg4gr6g >d4d4r>d4<d cc4cr6c <f4f4r>g4<g
+#IpL16Q7<g/>c/e<g+/>c/f<g/>c/e <g+/>c/f< a+/>c/f<a/>c/f<g/>c/eL8<f/a/>c<g/b/>d
+#
+#IlL2Q7>g10>A1dA<b>c6<b3r1b4a3r1A1a3Ag3ad+1d1c6r8Q8d+8Q7e16g+4a4b8b1a1Q8g8Q7arg4a
+#IbQ4O2L2 ee4er6e <a4a4r>a4<a >dd4dr6d <a+4a+4r>a+4<a+ aa4ar6a g+4g+4r>g+4<g+ gg4gr6g g4g4r>g4<g
+#IpL16Q7<b/>d/g<g/>c/e<a/>c/f<g+/>c/f<g/>c/e<f+/>c/e<f/>c/e<g/b/>d
 
 )";
 
@@ -121,7 +124,7 @@ std::array<Voice, POLY> voices;
 std::array<Instrument, 128> instruments;
 std::vector<float> envelops[] = {
    { 0, 0 },
-   { -2, -1.5, -1, -0.66, -0.33, 0, 5 },
+   { -2, -1.66, -1.33, -1, -0.66, -0.33, 0, 6 },
    { -2, -2, -2, -2, 0, 0, 0, 0, -2, 8 },
    { 0, 0, 3, 3, 7, 7, 10, 10, 0},
    { 0, 0, 4, 4, 7, 7, 12, 12, 0},
@@ -131,7 +134,7 @@ void init_voices() {
    instruments[ 0 ] = { 1,   PULSE, 0.5, 0,   { 0.01,  0.5, 0.9999,   0.9992 }  };
    instruments['l'] = { 0.8, PULSE, 0.2, 0.2, { 0.01,  0.5, 0.9999,   0.9992 }, 0.15, 0.09, false, 0, 0, 0.3 };
    instruments['p'] = { 0.3, PULSE, 0.3,-0.3, { 0.001, 0.0, 0.999992, 0.9999 }, 0.3,  0.05 };
-   instruments['b'] = { 1.8, PULSE, 0.3, 0.1, { 0.01,  0.5, 0.9998,   0.9992 }, 0, 0.2, true, 1.1, -3.5 };
+   instruments['b'] = { 1.8, PULSE, 0.3, 0.1, { 0.01,  0.5, 0.9998,   0.9992 }, 0, 0.2, true, 0.9, -4.5 };
 }
 
 
